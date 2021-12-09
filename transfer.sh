f096bc8201c4d70aa7568a6be4d38afb3c3fd4b7
willis <willis@hhi.fraunhofer.de>
2021-12-09 11:35:46 +0100

    transfer bug + baseline validated

--- transfer.sh
+++ transfer.sh
@@ -1,51 +1,38 @@
-# for f1 in 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 0
-#     do
-#     for f2 in 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 0
-#         do
-#         python transfer.py \
-#         --dataset_to CIFAR10Distorted \
-#         --network Unet \
-#         --model_from models/CIFAR10_resnet34.ckpt \
-#         --cuda \
-#         --save_best \
-#         --lr 0.01 \
-#         --f_stats 1e-6 \
-#         --f_norm $f1 \
-#         --f_tv $f2 \
-#         --size 2048 \
-#         --batch_size 256 \
-#         --num_epochs 100 \
-#         --reset
-#     done
-# done
+run='python -m IPython --no-banner --no-confirm-exit'
 
-for lr in 0.1
+for size in 128 512 2048 8192 32768
     do
-    # for f in 0 1e-7 3e-7 7e-7 1e-6 3e-6
-    for f_st in 0 1e-7 1e-6 1e-5 1e-4
-        do
-        python transfer.py \
-        --dataset_to MNIST \
-        --network Unet \
-        --model_from models/SVHN_resnet34.ckpt \
-        --cuda \
-        --lr $lr \
-        --f_stats $f_st \
-        --size 2048 \
-        --batch_size 256 \
-        --num_epochs 500 \
-        --reset
 
-        # --resume_training
+    $run transfer.py -- \
+    --dataset_to PBCBarcelona-4x \
+    --network Unet \
+    --model_from models/Cytomorphology-4x_Resnet34.ckpt \
+    --cuda \
+    --lr 1e-3 \
+    --f_stats 1e-7 \
+    --size $size \
+    --batch_size 64 \
+    --num_epochs 30 \
 
-        # --dataset_to CIFAR10Distorted \
-        # --model_from models/CIFAR10_resnet34.ckpt \
-    done
+    # --reset \
+
 done
 
-# --size 4096 \
-# --unsupervised
 
-# --dataset PBCBarcelona \
-# --model_ckpt models/PBCBarcelona_resnet34_no_augmentation \
-# --batch_size 4 \
+for lr in 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7
+    do
+    for f_stats in 0 1e-7
+        do
+
+        $run transfer.py -- \
+        --dataset_to PBCBarcelona-4x \
+        --network Unet \
+        --model_from models/Cytomorphology_4x_resnet34.ckpt \
+        --cuda \
+        --lr $lr \
+        --f_stats $f_stats \
+        --batch_size 64 \
+        --num_epochs 30
+
+    done
+done
